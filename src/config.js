0e8af5d43a21f8da1f9e59f1f38dcdd0a7ea44f6
007353 <shivang.oneknotone@gmail.com>
2025-06-11 17:51:13 +0530

    first commit

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  API_URL: 'https://your-render-deployment-url.onrender.com'  // Replace with your actual Render deployment URL
+  API_URL: 'https://flask-2d-to-3d.onrender.com'  // Replace with your actual Render deployment URL
 };
 
 export default config; 
